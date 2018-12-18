e185c2b1d8dde094c6b342bc612a587deb6d522f
Shrekinator19 <43785257+Shrekinator19@users.noreply.github.com>
2018-12-18 09:53:15 -0500

    commit three

---
+++ problem 1.py
@@ -0,0 +1,11 @@
+print('_'*65)
+print('Life Decision Bot:')
+print()
+raining = input('Is it raining outside?')
+if raining == 'Yes':
+	print ('Stay at home!')
+	print('Maybe binge watch a new show on Netflix?')
+else:
+	print('Go out and do something!')
+	print('Maybe go on a hike!')
+print('_'*65)
